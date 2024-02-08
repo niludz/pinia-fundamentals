49f801ff8e1b23eb617fe6414bf482ca2dcf87a6
rjwang <rjwang@zjft.com>
2024-02-08 13:06:48 +0800

    ...

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,20 @@
+<script setup>
+import TodoList from './TodoList.vue'
+import TodoForm from './TodoForm.vue'
+</script>
+
+<template>
+  <div class="todo-app">
+    <h1>TODO APP (parent)</h1>
+    <TodoForm></TodoForm>
+    <TodoList></TodoList>
+    </div>
+</template>
+<style scoped>
+.todo-app {
+  padding: 5vh;
+  min-height: 100vh;
+  width: 30vw;
+  background-color: #f7f9fc;
+}
+</style>
